e3866f070ce86aadffda3640ee754e6b9ae0a82b
divineion <najima.guermoudi@gmail.com>
2024-12-04 16:09:26 +0100

    src/com/divineion/utils/ApplicationMessages.java :
     - add string constants to manage error or information messages

---
+++ src/com/divineion/utils/ApplicationMessages.java
@@ -0,0 +1,15 @@
+package com.divineion.utils;
+
+public class ApplicationMessages {
+	public static final String ANIMAL_NAME_ERROR = "Erreur -> nom de l'animal : ";
+	public static final String ANIMAL_AGE_ERROR = "Erreur -> âge de l'animal : ";
+	public static final String DOG_BREED_ERROR = "Erreur -> race du chien : ";
+	public static final String BIRD_FEATHERS_ERROR = "Erreur -> nombre de plumes : ";	
+	
+	public static final String POSITIVE_VALUE_REQUIRED = "Le nombre saisi doit être un nombre entier positif.";
+	public static final String VALUE_CANNOT_BE_EMPTY = "Le champ ne peut pas être vide.";
+	public static final String VALUE_TOO_SHORT = "Le nombre de caractères saisis est insuffisant.";
+	public static final  String CAPITALIZED_REQUIRED = "La saisie doit commencer par une majuscule";
+	
+	public static final String END_OF_PROGRAMM = "Fin du programme";
+}
