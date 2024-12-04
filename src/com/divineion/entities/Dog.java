f54e893039972ec5bf5b01833b5700524a886e1b
divineion <najima.guermoudi@gmail.com>
2024-12-04 15:50:00 +0100

    feat: src/com/divineion/entities/Dog.java :
     - create Dog class as subclass of Animal
     - add breed property with validation in constructor
     - override feed() and sleep() methods
     - implement bark() method in behave() override

---
+++ src/com/divineion/entities/Dog.java
@@ -0,0 +1,59 @@
+package com.divineion.entities;
+
+import com.divineion.utils.exceptions.AnimalBreedException;
+import com.divineion.utils.exceptions.EmptyValueException;
+import com.divineion.utils.exceptions.NameFormatException;
+import com.divineion.utils.exceptions.PositiveValueRequiredException;
+import com.divineion.utils.exceptions.TooShortValueException;
+import com.divineion.utils.validators.DogValidator;
+
+public class Dog extends Animal {
+	private String breed;
+	private String food;
+	
+	public Dog(final String name, final String species, final int age, final String breed) throws PositiveValueRequiredException, AnimalBreedException, EmptyValueException, TooShortValueException, NameFormatException {
+		super(name, species, age);
+		DogValidator.validateDogBreed(breed);
+		this.breed = breed;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+	
+	public void bark() {
+		System.out.println("J'aboie");
+	}
+	
+	@Override
+	public void feed(String food) {
+		System.out.println("Je peux manger " + food);
+	}
+	
+	@Override
+	public void sleep() {
+		System.out.println("Je dors dans une niche");
+	}
+	
+	@Override
+	public String toString() {
+		 return super.toString() + " et je suis de race " + breed;
+	}
+	
+	@Override
+	public void behave() {
+		bark();
+	}
+}
