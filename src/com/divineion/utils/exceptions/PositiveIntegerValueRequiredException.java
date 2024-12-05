3ca633289b2f49b89747a06822dc88d226501a98
divineion <najima.guermoudi@gmail.com>
2024-12-05 15:49:07 +0100

    sc/com/divineion/utils/exceptions/PositiveIntegerValueRequiredException.java :
    rename PositiveValueRequiredException to
    PositiveIntegerValueRequiredException

--- src/com/divineion/utils/exceptions/PositiveIntegerValueRequiredException.java
+++ src/com/divineion/utils/exceptions/PositiveIntegerValueRequiredException.java
@@ -3,9 +3,9 @@ package com.divineion.utils.exceptions;
 	 * This class represents a custom exception used to ensure that an input value is positive
 	 */
 @SuppressWarnings("serial")
-public class PositiveValueRequiredException extends Exception {
+public class PositiveIntegerValueRequiredException extends Exception {
 	
-	public PositiveValueRequiredException(String message) {
+	public PositiveIntegerValueRequiredException(String message) {
 		super(message);
 	}
 
