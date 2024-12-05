9915c58d1ef0ed5ca979a3eba610d46e4034561a
divineion <najima.guermoudi@gmail.com>
2024-12-05 16:35:45 +0100

    refactor : src/com/divineion/entities/Dog.java :
     - move validation from constructor to setters to separate instantiation
    and validation logics and allow the program to validate each
    modification made to an existing object;
     - call setters within the constructor to ensure validation during an
    instantiation

--- src/com/divineion/entities/Dog.java
+++ src/com/divineion/entities/Dog.java
@@ -1,30 +1,32 @@
 package com.divineion.entities;
 
-import com.divineion.utils.exceptions.AnimalBreedException;
 import com.divineion.utils.exceptions.EmptyValueException;
 import com.divineion.utils.exceptions.NameFormatException;
-import com.divineion.utils.exceptions.PositiveValueRequiredException;
+import com.divineion.utils.exceptions.PositiveIntegerValueRequiredException;
 import com.divineion.utils.exceptions.TooShortValueException;
 import com.divineion.utils.validators.DogValidator;
 
 public class Dog extends Animal {
 	private String breed;
 	private String food;
-	
-	public Dog(final String name, final String species, final int age, final String breed) throws PositiveValueRequiredException, AnimalBreedException, EmptyValueException, TooShortValueException, NameFormatException {
+
+	public Dog(final String name, final String species, final int age, final String breed)
+			throws PositiveIntegerValueRequiredException, EmptyValueException, TooShortValueException,
+			NameFormatException {
 		super(name, species, age);
-		DogValidator.validateDogBreed(breed);
-		this.breed = breed;
+		setBreed(breed);
 	}
 
 	public String getBreed() {
 		return breed;
 	}
 
-	public void setBreed(String breed) {
+	public void setBreed(String breed) throws TooShortValueException {
+		DogValidator.validateDogBreed(breed);
+
 		this.breed = breed;
 	}
-	
+
 	public String getFood() {
 		return food;
 	}
@@ -32,26 +34,26 @@ public class Dog extends Animal {
 	public void setFood(String food) {
 		this.food = food;
 	}
-	
+
 	public void bark() {
 		System.out.println("J'aboie");
 	}
-	
+
 	@Override
 	public void feed(String food) {
 		System.out.println("Je peux manger " + food);
 	}
-	
+
 	@Override
 	public void sleep() {
 		System.out.println("Je dors dans une niche");
 	}
-	
+
 	@Override
 	public String toString() {
-		 return super.toString() + " et je suis de race " + breed;
+		return super.toString() + " et je suis de race " + breed;
 	}
-	
+
 	@Override
 	public void behave() {
 		bark();
